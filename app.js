5e71b5acd9aeedca76f3a30ef187288c5f106eab
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-03-21 05:55:12 +0700

    output data in mongodb database

--- app.js
+++ app.js
@@ -9,9 +9,52 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 
-app.get('/',function(req,res){
-    res.render('list');
+const todoSchema = {
+    todo : String
+};
+
+const Todo = new mongoose.model('Todo',todoSchema);
+
+const todo1 = new Todo({
+    todo : "Belajar React Js"
 });
+
+const todo2 = new Todo({
+    todo : "Belajar Vue Js"
+});
+
+const todo3 = new Todo({
+    todo : "Belajar Angular Js"
+});
+
+const defaultTodo = [todo1,todo2,todo3];
+
+
+
+app.get('/',function(req,res){
+
+    Todo.find({},function(err, ItemsTodo){
+        if(ItemsTodo.length === 0){
+            Todo.insertMany(defaultTodo,function(err){
+                if(err){
+                    console.log(err)
+                }else {
+                    console.log('successfully add todo !');
+
+                    res.redirect('/')
+                }
+            })
+        }else{
+            res.render('list', {title : "Today", todos : ItemsTodo});
+        }
+    });
+    
+});
+
+
+app.post('/deleteTodo',function(req,res){
+    console.log(req.body.todoId);
+})
 
 app.listen(3000,function(){
     console.log('Server running on port 3000');
